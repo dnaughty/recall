610772380b460662609422747b18b12df1c54319
DENIS NAUGHTON <mjolinor75@hotmail.comssh-keygen -t rsa -b 4096 -C mjolinor75@hotmail.com>
2018-11-04 13:05:31 -0500

    first commit

---
+++ main.rb
@@ -0,0 +1,18 @@
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/recall.db")
+
+class Note
+	include DataMapper::Resource
+	property :id, Serial
+	property :content, Text, :required => true
+	property :complete, Boolean, :rquired => true, :default => false
+	property :created_at, DateTime
+	property :updated_at, DateTime
+end
+
+DataMapper.finalize.auto_upgrade!
+
+get '/' do
+	@notes = Note.all : order => :id.desc
+	@title = "All Notes"
+	erb :home
+end
